3816db35abfdd269a2b403bc505e6dbfc13dfb7d
FAS1969 <fas1969@gmail.com>
2020-04-15 12:09:29 +0200

    *

--- src/_test.js
+++ src/_test.js
@@ -5,4 +5,14 @@
 
   console.log("prop" in o === o.hasOwnProperty("prop"));
   console.log("toString" in o === o.hasOwnProperty("toString"));
+
+  var obj = { a: 2 };
+  function f(aobj) {
+    aobj.a = 5;
+    console.log(aobj);
+    aobj = { a: 3 };
+    console.log(aobj);
+  }
+  f(obj);
+  console.log(obj);
 }
